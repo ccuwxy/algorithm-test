9a8734adfb0b3b377cd7fb57c4cc9f40bfff9b02
WangXinyu <wxy990212@126.com>
2020-04-01 11:21:27 +0800

    .

---
+++ test/src/nowcoder/ctrip2019/t22.java
@@ -0,0 +1,14 @@
+package nowcoder.ctrip2019;
+
+/**
+ * 设计一个数据结构，实现LRU Cache的功能(Least Recently Used – 最近最少使用缓存)。它支持如下2个操作： get 和 put。
+ *
+ * int get(int key) – 如果key已存在，则返回key对应的值value（始终大于0）；如果key不存在，则返回-1。
+ * void put(int key, int value) – 如果key不存在，将value插入；如果key已存在，则使用value替换原先已经存在的值。如果容量达到了限制，LRU Cache需要在插入新元素之前，将最近最少使用的元素删除。
+ *
+ * 请特别注意“使用”的定义：新插入或获取key视为被使用一次；而将已经存在的值替换更新，不算被使用。
+ *
+ * 限制：请在O(1)的时间复杂度内完成上述2个操作。
+ */
+public class t22 {
+}

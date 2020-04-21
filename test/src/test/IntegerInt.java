3255a1c77d4134012c4b8c5d07ffcdabea51628a
WangXinyu <wxy990212@126.com>
2020-04-21 15:19:18 +0800

    default about int and Integer

---
+++ test/src/test/IntegerInt.java
@@ -0,0 +1,13 @@
+package test;
+
+public class IntegerInt {
+    private static int a;
+    private static Integer aa;
+
+    public static void main(String[] args) {
+        int i = 0;
+        Integer ii;
+        System.out.println(a);
+        System.out.println(aa);
+    }
+}
